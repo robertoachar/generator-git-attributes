3fbd094443617aa15a32dab050b3834efe36bcc1
Roberto Achar <robertoachar@gmail.com>
2017-06-09 15:28:01 -0300

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const lib = require('../src');
+
+describe('TESTS', () => {
+
+  test('Basic test', (done) => {
+    lib.hello((err, message) => {
+      expect(err).toBeNull();
+      expect(message).toBe('It works!');
+
+      done();
+    });
+  });
+
+});
